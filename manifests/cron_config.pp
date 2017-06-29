540031db9fad100a3d80f91971d6e23df94d98fe
Jaime Gomez <jaimegdpr@users.noreply.github.com>
2017-06-29 10:27:05 +0200

    Adding ciemat-fetch-crl cron at boot

--- manifests/cron_config.pp
+++ manifests/cron_config.pp
@@ -5,20 +5,34 @@ class ciemat_certs::cron_config {
     $random_hour_plus_12h = $random_hour + 12
     $random_hour_plus_18h = $random_hour + 18
 
-    cron::job { 'ciemat-fetch-crl':
-        minute      => fqdn_rand(60, 'adding more randomness with this seed'),
-        hour        => "${random_hour},${random_hour_plus_6h},${random_hour_plus_12h},${random_hour_plus_18h}",
-        date        => '*',
-        month       => '*',
-        weekday     => '*',
-        user        => 'root',
-        command     => 'rsync -u --chmod=u+rw,g+r,o+r lcg01::glite/CRL/*  /etc/grid-security/certificates/  >> /var/log/fetch-crl-cron.log 2>&1',
+    cron::job::multiple { 'ciemat-fetch-crl':
+        jobs => [
+          {
+            minute      => fqdn_rand(60, 'adding more randomness with this seed'),
+            hour        => "${random_hour},${random_hour_plus_6h},${random_hour_plus_12h},${random_hour_plus_18h}",
+            date        => '*',
+            month       => '*',
+            weekday     => '*',
+            user        => 'root',
+            command     => 'rsync -u --chmod=u+rw,g+r,o+r lcg01::glite/CRL/*  /etc/grid-security/certificates/  >> /var/log/fetch-crl-cron.log 2>&1',
+            environment => [ 'MAILTO=root', 'PATH="/sbin:/bin:/usr/sbin:/usr/bin"' ],
+            description => 'Get crls from lcg01',
+          },
+          {
+            minute => '@reboot',
+            hour        => '',
+            date        => '',
+            month       => '',
+            weekday     => '',
+            user        => 'root',
+            command     => 'rsync -u --chmod=u+rw,g+r,o+r lcg01::glite/CRL/*  /etc/grid-security/certificates/  >> /var/log/fetch-crl-cron.log 2>&1',
+          },
+        ],
         environment => [ 'MAILTO=root', 'PATH="/sbin:/bin:/usr/sbin:/usr/bin"' ],
-        description => 'Get crls from lcg01',
     } 
 
-    file { '/etc/cron.d/fetch-crl':
-        ensure => absent,
+        file { '/etc/cron.d/fetch-crl':
+            ensure => absent,
     }
 
 }
